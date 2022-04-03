e264cca38b8ce8c7b5088e96300c7c2efa6a5c5a
Sefiyye <tu7l1lxat@code.edu.az>
2022-04-03 19:09:36 +0400

    Delegate task

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Delegate_Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] Product = { 1.5, 0.7, 2.3, 7.2, 3.4 };
+            Action<double[]> TotalAndCount = ProductTotalAndPrice;
+            ProductTotalAndPrice(Product);
+        }
+        public static void ProductTotalAndPrice(double[] arr)
+        {
+            int count = default(int);
+            double price = default(double);
+            foreach (double item in arr)
+            {
+                count++;
+                price += item;
+            }
+            Console.WriteLine($"Count: {count}, Price:{ price}");
+
+        }
+    }
+}
